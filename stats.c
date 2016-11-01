a5f5e98b5525ab6b1ccae773e9984977363bc18b
Sarah Yoon <syoon2@stuy.edu>
2016-10-31 23:02:07 -0400

    finish

--- stats.c
+++ stats.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <time.h>
 
+
 int main(){
   struct stat buf;
   int error = stat("stats.c", &buf);
@@ -19,6 +20,24 @@ int main(){
   printf("\t   %lfGB\n", buf.st_size/1000000000.0);
   
   printf("permissions: %o\n", buf.st_mode & 0777);
+
+  printf("permissions in ls -l format:\n");
+  
+  printf( (S_ISDIR(buf.st_mode)) ? "d" : "-");
+  
+  printf( (buf.st_mode & S_IRUSR) ? "r" : "-");
+  printf( (buf.st_mode & S_IWUSR) ? "w" : "-");
+  printf( (buf.st_mode & S_IXUSR) ? "x" : "-");
+  
+  printf( (buf.st_mode & S_IRGRP) ? "r" : "-");
+  printf( (buf.st_mode & S_IWGRP) ? "w" : "-");
+  printf( (buf.st_mode & S_IXGRP) ? "x" : "-");
+  
+  printf( (buf.st_mode & S_IROTH) ? "r" : "-");
+  printf( (buf.st_mode & S_IWOTH) ? "w" : "-");
+  printf( (buf.st_mode & S_IXOTH) ? "x" : "-");
+
+  printf("\n");
 
   //gmtime takes in the pointer to time which is st_atime and returns a struct
   //asctime takes the struct and returns the values into human readable stuff
